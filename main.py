d7d213fadcc48a27edbb59afbbd7c4f396433f4e
Ali Mehri <alimehridev@gmail.com>
2024-06-18 07:22:59 +0330

    GET /posts route is added to routes .

--- main.py
+++ main.py
@@ -4,4 +4,23 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello friend ."}
+    return {"message": "Welcome to my api"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {
+        "data": [
+            {
+                "id": 1,
+                "title": "This is first post",
+                "body": "Hello, this is first post and I wish you like it ."
+            },
+            {
+                "id": 2,
+                "title": "Second post title",
+                "body": "In second post, I wanna talk about some thing ..."
+            }
+        ],
+        "status": "success"
+    }
