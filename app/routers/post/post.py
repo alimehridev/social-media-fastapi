a6ed8dd728c7d1a32b18cbae5590356223a61470
Ali Mehri <alimehridev@gmail.com>
2024-06-25 16:57:34 +0330

    deleting and updating post are based on owner_id and a user can only delete and update his own psots .

--- app/routers/post/post.py
+++ app/routers/post/post.py
@@ -2,7 +2,7 @@
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 from typing import List
 # Database imports
-from app import models
+from . import models
 from app.routers.post import schemas
 from app.database import get_db
 from sqlalchemy.orm import Session
@@ -53,13 +53,15 @@ def post(id: int, db: Session = Depends(get_db), current_user : User = Depends(o
 @router.delete("/{id}")
 def delete_post(id: int, db: Session = Depends(get_db), current_user : User = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
-
     if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id={id} does not exit to be deleted .")
-    post.delete(synchronize_session=False)
-    db.commit()
+    if post.first().owner_id == current_user.id:
+        post.delete(synchronize_session=False)
+        db.commit()
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not allowed to delete this post .")
 
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
     
 
 # Update a Post Function
@@ -69,7 +71,8 @@ def update_post(id: int, body: schemas.PostCreate, db: Session = Depends(get_db)
 
     if post_query.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id={id} does not exist .")
-    
+    if post_query.first().owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="You are not allowed to update this post .")
     post_query.update(body.model_dump(), synchronize_session=False)
     db.commit()
     return {
