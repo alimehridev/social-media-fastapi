2155c30663414ed1bdca0cb0a687cfcae6edd382
Ali Mehri <alimehridev@gmail.com>
2024-06-25 16:32:48 +0330

    owner_id column is added to posts table as a foreign key to users.id

--- app/routers/post/models.py
+++ app/routers/post/models.py
@@ -1,5 +1,5 @@
 from app.database import Base
-from sqlalchemy import Column, Integer, String, Text, Boolean
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 
@@ -10,4 +10,5 @@ class Post(Base):
     title = Column("title", String(68), nullable=False)
     content  = Column("content", Text, nullable=False)
     published = Column("published", Boolean, nullable=False, server_default="TRUE")
+    owner_id = Column("owner_id", ForeignKey("users.id", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
     created_at = Column("created_at", TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
