32c7465787dfdb8c95ed3ede8b57ec6a373f1079
Ali Mehri <alimehridev@gmail.com>
2024-06-26 14:16:25 +0330

    No reason :)

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from app.config import Settings
 
 settings = Settings()
 #SQLALCHEMY_DATABASE_URL = 'postgressql://<username>:<password>@<ip-address/hostname>/<database_name>'
-SQLALCHEMY_DATABASE_URL = 'postgresql://' + settings.DATABASE_USERNAME + ':' + settings.DATABASE_PASSWORD + '@' + settings.DATABASE_HOSTNAME + '/' + settings.DATABASE_NAME
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, bind=engine, autocommit=False)
 Base = declarative_base()
