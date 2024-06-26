6d3ecc87d99453fabaaa8a0d75fd971f0d1a3ee8
Ali Mehri <alimehridev@gmail.com>
2024-06-26 14:05:21 +0330

    Database information in database.py now is based on settings of .env file .

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from app.config import Settings
 
-
+settings = Settings()
 #SQLALCHEMY_DATABASE_URL = 'postgressql://<username>:<password>@<ip-address/hostname>/<database_name>'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:mysecretpassword@localhost/social-media'
+SQLALCHEMY_DATABASE_URL = 'postgresql://' + settings.DATABASE_USERNAME + ':' + settings.DATABASE_PASSWORD + '@' + settings.DATABASE_HOSTNAME + '/' + settings.DATABASE_NAME
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, bind=engine, autocommit=False)
 Base = declarative_base()
